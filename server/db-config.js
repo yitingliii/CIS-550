9a14bbf010224ccaaf443a6402ac107239c4bcdd
yitingliii <ytingli@seas.upenn.edu>
2024-11-10 13:26:04 -0500

    Add files via upload

---
+++ server/db-config.js
@@ -0,0 +1,8 @@
+// Private. Will not be included in submission
+module.exports = {
+  host: "",
+  port: "3306",
+  user : "",
+  password: "",
+  database: "Friends_and_Family_DB"
+};
---
+++ server/routes.js
@@ -0,0 +1,48 @@
+var config = require('./db-config.js');
+var mysql = require('mysql');
+
+config.connectionLimit = 10;
+var connection = mysql.createPool(config);
+
+/* -------------------------------------------------- */
+/* ------------------- Route Handlers --------------- */
+/* -------------------------------------------------- */
+
+/* ---- (Dashboard) ---- */
+function getAllPeople(req, res) {
+  var query = `
+    SELECT login, name, birthyear
+    FROM Person;
+  `;
+  connection.query(query, function (err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+};
+
+/* ---- Part 2 (FindFriends) ---- */
+function getFriends(req, res) {
+  var inputLogin = req.params.login;
+
+  console.log("input received:" + inputLogin);
+
+  // TODO: (3) - Edit query below
+  var query = `
+    <INSERT QUERY HERE, using inputLogin>;
+  `;
+  connection.query(query, function (err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      console.log(rows);
+      res.json(rows);
+    }
+  });
+};
+
+// The exported functions, which can be accessed in index.js.
+module.exports = {
+  getAllPeople: getAllPeople,
+  getFriends: getFriends
+}
